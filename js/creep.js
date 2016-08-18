eaa191f37bc00f6e813cc57ad4c2be4304f5f0ef
Kevin Wang <kevinwang@kevinwang.us>
2016-08-17 22:55:28 -0700

    an idol in my eyes

---
+++ js/creep.js
@@ -0,0 +1,109 @@
+Enemy = function(game, x, y, key, waypoints, velocity_inverse){
+	Phaser.Sprite.call(this, game, x, y, key);	
+	this.velocity_inverse = velocity_inverse;
+	this.waypoints = waypoints;
+	//this.anchor.setTo(0.5, 0.5);
+}
+Enemy.prototype = Object.create(Phaser.Sprite.prototype);
+Enemy.prototype.constructor = Enemy;
+Enemy.prototype.spawn = function(){
+	this.exists = true;
+	this.tween.start();
+}
+
+BlandEnemy = function(game){
+}
+
+Wave = function(game, EnemyClass, amount, delay, interval, waypoints, velocity_inverse){
+	Phaser.Group.call(this, game);
+	//this.anchor.setTo(0.5, 0.5);
+	this.classType = EnemyClass;
+	this.amount = amount;
+	this.delay = delay;
+	this.interval = interval;
+	this.waypoints = waypoints;
+	for(var i = 0; i < amount; i++){
+		enemy = this.create(0, 0, 'dude', 0, false, waypoints, velocity_inverse);
+		enemy.anchor.setTo(0.5, 0.5);
+
+		enemy.x = waypoints[0][0];
+		enemy.y = waypoints[0][1];
+		enemy.rotation = Phaser.Math.angleBetween(waypoints[0][0], waypoints[0][1], waypoints[1][0], waypoints[1][1]);
+
+		tween = game.add.tween(enemy);
+		//console.assert(waypoints.length > 1);
+		//var SpeedGamingConstant = 10//30 //Kappa
+		enemy.tween = tween;
+		for(var wp = 1; wp < waypoints.length; wp++){
+			distance = Math.sqrt((waypoints[wp][0] - waypoints[wp-1][0]) * (waypoints[wp][0] - waypoints[wp-1][0]) + (waypoints[wp][1] - waypoints[wp-1][1]) * (waypoints[wp][1] - waypoints[wp-1][1]));
+			tween.to({ x: waypoints[wp][0], y: waypoints[wp][1] }, distance*velocity_inverse, "Linear");
+			if(wp != waypoints.length -1){
+				rotation = Phaser.Math.angleBetween(waypoints[wp][0], waypoints[wp][1], waypoints[wp+1][0], waypoints[wp+1][1]);
+				prevrot = ((wp == 0) ? enemy.rotation : Phaser.Math.angleBetween(waypoints[wp-1][0], waypoints[wp-1][1], waypoints[wp][0], waypoints[wp][1])); //if performance is ever an issue, then find out where this is stored, or store it.  i'm lazy so this is fine.  even though writing this comment probably took longer than it would have to just cache it.  but whetutevr.
+				ntween = game.add.tween(enemy).to({angle: getShortestAngle(prevrot, rotation)}, 150, "Linear");
+				tween.chain(ntween);
+				tween = ntween;
+				ntween = game.add.tween(enemy)
+				tween.chain(ntween);
+    			tween = ntween;
+    		} else {
+    			tween.onComplete.add(function(thing, tweenref){thing.kill();});
+    		}
+    	}
+	}
+	if(delay){
+		game.time.events.add(delay, this.start, this);
+	} else {
+		this.start();
+	}
+}
+Wave.prototype = Object.create(Phaser.Group.prototype);
+Wave.prototype.constructor = Wave;
+Wave.prototype.start = function(){
+	this.spawned = 0;
+	game.time.events.repeat(this.interval, this.amount, function(){this.children[this.spawned].spawn();this.spawned++;}, this);
+}
+Wave.prototype.create = function (x, y, key, frame, exists, waypoints, velocity_inverse) {
+
+	var args = [];
+
+    if (arguments.length > 5)
+    {
+        for (var i = 5; i < arguments.length; i++)
+        {
+            args.push(arguments[i]);
+        }
+	}
+
+    if (exists === undefined) { exists = true; }
+
+    var child = new this.classType(this.game, x, y, key, frame, waypoints, velocity_inverse);
+
+    child.exists = exists;
+    child.visible = exists;
+    child.alive = exists;
+
+    this.addChild(child);
+
+    child.z = this.children.length;
+
+    if (this.enableBody)
+    {
+        this.game.physics.enable(child, this.physicsBodyType, this.enableBodyDebug);
+    }
+
+    if (child.events)
+    {
+        child.events.onAddedToGroup$dispatch(child, this);
+    }
+
+    if (this.cursor === null)
+    {
+        this.cursor = child;
+    }
+
+    return child;
+
+};
+
+
---
+++ js/tower.js
@@ -0,0 +1,95 @@
+Tower = function(game, tilex, tiley, key){
+	x = tilex*70;
+	y = tiley*70;
+	Phaser.Sprite.call(this, game, x, y, key);	
+	//this.anchor.setTo(0.5, 0.5);
+}
+Tower.prototype = Object.create(Phaser.Sprite.prototype);
+Tower.prototype.constructor = Tower;
+
+FoxTower = function(game, tilex, tiley){
+	Tower.call(this, game, tilex, tiley, "fox");
+	this.armedState = "GUNSIN";  //"GUNSOUT", "ARMING", "DISARMING"
+	this.timeSinceLastShot = 0;  // //this is not technically true, but ugh I can't think of a better way to do this
+
+	this.animationArm = this.animations.add('arm');
+	this.animationArm.onComplete.add(function(){this.armedState="GUNSOUT";this.fire();}, this);
+}
+FoxTower.prototype = Object.create(Tower.prototype);
+FoxTower.prototype.constructor = FoxTower;
+FoxTower.prototype.armUrself = function(){
+	//animation;
+	this.armedState = "ARMING";
+	this.animationArm.play(30, false);
+}
+FoxTower.prototype.disarmUrself = function(){
+	this.armedState = "DISARMING";
+	this.animationArm.play(30, false); //should be reversed obviously
+}
+FoxTower.prototype.findTarget = function(){
+	for(i = 0; i < wave1.children.length; i++){
+		enemy = wave1.children[i];
+		if(Phaser.Math.distance(enemy.x, enemy.y, this.x, this.y) < 150){//100
+			this.target = wave1.children[i];
+			return this.target;
+		}
+	}
+}
+FoxTower.prototype.update = function(){
+	this.target = this.findTarget();
+	this.timeSinceLastShot++;
+	//rotate towards that target
+	if(this.target == null){ //or whatever the idiomatic way to do this in js is.
+		if(this.timeSinceLastShot > 40){
+			this.disarmUrself();
+		}
+	} else {
+		if(this.armedState == "GUNSIN"){
+			this.armUrself();
+		} else if(this.armedState == "GUNSOUT" && this.timeSinceLastShot>3){
+			this.fire();
+		}
+	}
+}
+FoxTower.prototype.fire = function(){
+	//shoot at where the enemy will be
+	
+	if(this.target == null){ 
+		//THERE SHOULD BE SOMETHING HERE.  most common way this line is reached is when 
+		//fox arms himself and then after the arming animation, there is no longer a target.
+		this.timeSInceLastShot = 0; //? i guess
+	} else {
+		this.timeSinceLastShot = 0;
+				game.add.existing(new FoxLaser(game, this.x, this.y, this.target.x, this.target.y));
+	}
+	console.log("bam!");
+}
+
+StraightProjectile = function(game, x0, y0, xf, yf, velocity, key){
+	//this.vx = vx; //x velocity
+	//this.vy = vy; 
+	//this.xf = xf; //final x position
+	//this.yf = yf;
+	distance = Math.sqrt((xf-x0)*(xf-x0) + (yf - y0)*(yf-y0));
+	Phaser.Sprite.call(this, game, x0, y0, key);
+	this.anchor.setTo(0.5, 0);
+	this.rotation = Phaser.Math.angleBetween(x0, y0, xf, yf) + 3.14/2;
+	tween = game.add.tween(this).to({ x: xf, y: yf}, distance*velocity, "Linear");
+	tween.start();
+}
+StraightProjectile.prototype = Object.create(Phaser.Sprite.prototype);
+StraightProjectile.prototype.constructor = StraightProjectile;
+//StraightProjectile.prototype.update = function(){
+//}
+FoxLaser = function(game, x0, y0, xf, yf){
+	StraightProjectile.call(this, game, x0, y0, xf, yf, 2, "foxlaser");
+}
+FoxLaser.prototype = Object.create(StraightProjectile.prototype);
+FoxLaser.prototype.constructor = FoxLaser;
+
+//each tower has 3 options ( i saw this in a TD I played before):
+//target options: "farthest along the path", "least farthest along the path", "lowest health", "highest health", etc.
+//^maybe not?  adds extra difficulty.  Depends on where you want this game to be on the casual----depth tradeoff spectrum
+//^nah fuck that.  fuck casuals.  we goin all in boysss
+//althoug
+//one of the fox upgrades is like upsmash and it's like a culling blade, melee range.
